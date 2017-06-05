333f7667ac7a7a6a0aed8eb61555c49dfb122ccc
AndrewBorst <aborst@iu.edu>
2017-06-04 21:57:12 -0400

    media

--- config/settings/production.py
+++ config/settings/production.py
@@ -76,10 +76,10 @@ INSTALLED_APPS += (
     'storages',
 )
 DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'    
-
+STATICFILES_STORAGE = DEFAULT_FILE_STORAGE
 # URL that handles the media served from MEDIA_ROOT, used for managing
 # stored files.
-MEDIA_URL = 'https://s3.amazonaws.com/%s/' % AWS_STORAGE_BUCKET_NAME
+MEDIA_URL = '/static/'
 
 
 # EMAIL
