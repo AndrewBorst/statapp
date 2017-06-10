b0a3dfb7bcdb261ec363d482dfeea3f14b445f60
AndrewBorst <aborst@iu.edu>
2017-06-10 17:49:52 -0400

    cert again

--- statapp/views.py
+++ statapp/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def dcv_validation(request):
-    return HttpResponse(open('static/B7061121B6C0142AE02D66B71F847186.txt').read())
+    return HttpResponse(open('/opt/statapp/statapp/statapp/static/B7061121B6C0142AE02D66B71F847186.txt').read())
